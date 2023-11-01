b19f7242fdc1e7a9c7b580b9406b1adfa796189a
unknown <cristian.ibanez@facturaenlinea.cl>
2023-11-01 02:18:12 -0300

    modificacion de ruta imagen call para import

--- src/components/home/CallingImg.jsx
+++ src/components/home/CallingImg.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import callimg from '/public/callingimg.webp'
 
 export const CallingImg = () => {
   return (
     <>
       <div  >
-        <img id='call-img' src='/public/callingimg.webp' />
+        <img id='call-img' src={callimg} />
       </div>
     </>
   )
