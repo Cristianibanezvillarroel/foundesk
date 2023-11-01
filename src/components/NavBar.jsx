fed3699e042fa49a5b80d78a71bdcdd7b26dd318
unknown <cristian.ibanez@facturaenlinea.cl>
2023-10-31 23:21:19 -0300

    cambio de imagen logo fondo transparente

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-import logo from '/public/logo_fw.png';
+import logo from '/public/logo_ft.png';
 
 
 export const NavBar = () => {
