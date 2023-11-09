6a8bc0ac8a229e291f78927587c075600ffffc86
unknown <cristian.ibanez@facturaenlinea.cl>
2023-11-09 12:28:03 -0300

    modificacion de ubicacion de llaves para la imagen del carrusel. Se puso antes del div que la contiene

--- src/components/home/CustomerCarruselImg.jsx
+++ src/components/home/CustomerCarruselImg.jsx
@@ -10,12 +10,12 @@ export const CustomerCarruselImg = () => {
   return (
     <>
       <div className='row py-5 mx-5'>
-        <div className='col xs-6 col-md-6 d-flex flex-column justify-content-between'>
-          {ListFiltrada.map(content => content.items.map(
+      {ListFiltrada.map(content => content.items.map(
             item =>
-              <img className="d-block w-100" id='customercarrusel-img' src={item.imagen} />
-          ))}
+        <div className='col xs-6 col-md-6 d-flex flex-column justify-content-between'>
+              <img className="d-block w-100" id='customercarrusel-img' src={item.imagen} />          
         </div>
+        ))}
         <div className='col xs-6 col-md-6 d-flex flex-column align-text-center' >
           <div style={{ fontSize: 'small' }}>
             <p>“Yo creo que el mayor valor agregado que tiene Foundesk es que he podido coordinar y controlar como nunca antes los aspectos impositivos de mi empresa.”</p>
