8654a5f444d3dc74409e3a3712ca44adff3967ea
unknown <cristian.ibanez@facturaenlinea.cl>
2023-12-01 11:22:58 -0300

    agrega tabs

--- src/boundlepages/BoundleCourses.jsx
+++ src/boundlepages/BoundleCourses.jsx
@@ -1,150 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Container } from 'react-bootstrap'
-import Dropdown from 'react-bootstrap/Dropdown'
-import { CoursesCards } from '../components/courses/CoursesCards.jsx'
-import { PaginationControl } from 'react-bootstrap-pagination-control'
-import { useParams } from 'react-router-dom'
-import { coursesCategoriesService } from '../services/coursescategories.js'
-import { coursesService } from '../services/courses.js'
+import React, { useState } from 'react'
+import { Container, Tab, Tabs } from 'react-bootstrap'
+import { BoundleCoursesAll } from '../boundlecomponents/BoundleCoursesAll'
 
 export const BoundleCourses = () => {
-
-    const {category} = useParams()
-    const [coursesSelect, setCoursesSelect] = useState(category ? category : 'Todos')
-    const [size, setSize] = useState(0)
-    const ListSize = (inputValue) => {
-        setSize(inputValue)
-    };
-
-    const goToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    };
-
-    const [page, setPage] = useState(1)
-    const [limit, setLimit] = useState(2)
-    const [data, setData] = useState([])
-
-    const [dataCoursesTotal, setDataCoursesTotal] = useState([])
-
-
-    useEffect(() => {
-        setTimeout(() => {
-            getCategoriesV1()
-            getDataV1(coursesSelect)
-        }, 100);
-    }, [])
-
-    const getCategoriesV1 = async () => {
-
-        const dataService = {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*'
-            }
-          }
-
-        const responseData = await coursesCategoriesService(dataService)       
-
-        const ListFiltrada = responseData.map(List => List.items.map(
-            item => item
-        ))
-        let itemsArray = ListFiltrada[0]
-        setDataCoursesTotal(itemsArray)
-    }
-
-    const getDataV1 = async (coursesSelect) => {
-
-        const dataService = {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*'
-            }
-          }
-
-        const responseData = await coursesService(dataService)
-
-        const ListFiltrada = responseData.filter(List => {
-            return List.message == 'Courses';
-        })
-
-        let arrayItems = []
-        const ListFiltradaData = responseData.map(ListV1 => {
-            return coursesSelect == 'Todos' ?
-
-                ListFiltrada.forEach(function (item) {
-                    let itemsObject = item.items
-                    for (let i = 0; i < itemsObject.length; i++) {
-                        arrayItems.push(itemsObject[i])
-                    }
-                })
-
-                :
-                ListFiltrada.forEach(function (item) {
-                    let itemsObject = item.items.filter(
-                        item => item.categoria == coursesSelect
-                    )
-                    for (let i = 0; i < itemsObject.length; i++) {
-                        arrayItems.push(itemsObject[i])
-                    }
-                })
-
-        })
-        console.log(arrayItems)
-        setData({ arrayItems })
-    }
+    const [key, setKey] = useState("Todos")
 
     return (
-        <Container>
-            <div id='courses-head'>Cursos online </div>
-            <div id='courses-selected'>{coursesSelect}</div>
-            <div className='courses-line-count'>
-                <div id='courses-line-count-left'>
-                    {size} Resultados</div>
-                <div id='courses-line-count-right'>
-                    <div id='courses-line-count-right-order'>ORDENAR POR</div>
-                    <div >
-                        <Dropdown>
-                            <Dropdown.Toggle variant="" id="courses-line-count-right-dropdown">
-                                {coursesSelect}
-                            </Dropdown.Toggle>
-
-                            <Dropdown.Menu>
-                                {dataCoursesTotal.map(content =>
-                                    <Dropdown.Item onClick={() => { setCoursesSelect(content.categoria), setPage(1), getDataV1(content.categoria) }}>{content.categoria}</Dropdown.Item>
-                                )}
-                                <Dropdown.Divider />
-                                <Dropdown.Item onClick={() => { setCoursesSelect('Todos'), setPage(1),getDataV1('Todos') }}>Todos</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <CoursesCards ListSize={ListSize} page={page} limit={limit} data={data} />
-            </div>
-            <div id='courses-backtoup'>
-                <Button variant='light' onClick={goToTop}>
-                    <span>&#8743;</span><span>&#160;&#160;</span>
-                    Volver arriba
-                </Button>
-            </div>
-            <div>
-                <PaginationControl
-                    page={page}
-                    between={4}
-                    total={size}
-                    limit={2}
-                    changePage={(page) => {
-                        setPage(page)
-                    }}
-                    ellipsis={1}
-                />
-            </div>
-        </Container>
+        <>
+            <Container>
+                <div id='courses-head'>Cursos online </div>
+                <Tabs
+                    id="controlled-tab-example"
+                    activeKey={key}
+                    onSelect={(k) => setKey(k)}
+                    className="mb-3">
+                    <Tab eventKey="Todos" title="Todos">
+                        Tab content for Home
+                    </Tab>
+                    <Tab eventKey="MisCursos" title="Mis Cursos">
+                        Tab content for Profile
+                    </Tab>
+                </Tabs>
+                {key == "Todos" ?
+                    <BoundleCoursesAll />
+                    :
+                    <p>Mis cursos</p>
+                }
+            </Container>
+        </>
     )
 }
