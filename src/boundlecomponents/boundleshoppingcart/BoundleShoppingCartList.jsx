aea6959789346176ead8a85fc974ef8d55be26e0
unknown <cristian.ibanez@facturaenlinea.cl>
2023-12-02 23:52:45 -0300

    agrega list group y toast a detalle de carro de compra

--- src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartList.jsx
+++ src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartList.jsx
@@ -1,16 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShoppingContext } from '../../context/ShoppingContext'
-import { Row } from 'react-bootstrap'
+import { ListGroup, Row, Toast } from 'react-bootstrap'
 
 export const BoundleShoppingCartList = () => {
   const { shoppingCount, setShoppingCount } = useContext(ShoppingContext)
-  const [ arrayStorage, setArrayStorage ] = useState([])
+  const [arrayStorage, setArrayStorage] = useState([])
 
   useEffect(() => {
     ShoppingListStart()
   }, [])
 
-  
+
   const ShoppingListStart = async () => {
     let shoppingList = []
     const ShoppingListGet = await localStorage.getItem('shoppingList')
@@ -20,20 +20,31 @@ export const BoundleShoppingCartList = () => {
       setShoppingCount(null)
     } else {
       console.log('storage con registros')
-      shoppingList = JSON.parse(ShoppingListGet) 
+      shoppingList = JSON.parse(ShoppingListGet)
       let shoppingListSize = shoppingList.length
       setShoppingCount(shoppingListSize)
       setArrayStorage(shoppingList)
-      console.log(shoppingListSize)
-      console.log(typeof shoppingList)
+
     }
   }
   return (
     <>
-    <Row>
-      {arrayStorage.map(content => 
-        <div>{content.title}</div>)}  
-    </Row>
-    </>    
+      <Row>
+        {arrayStorage.map(content =>
+          <ListGroup as="ol" numbered>
+            <ListGroup.Item as="li">
+              <Toast>
+                <Toast.Header>
+                  <img src={content.imagen} className="rounded me-2" alt="" />
+                  <strong className="me-auto">{content.author}</strong>
+                  <small>{content.price}</small>
+                </Toast.Header>
+                <Toast.Body>{content.title}</Toast.Body>
+              </Toast>
+            </ListGroup.Item>
+          </ListGroup>
+        )}
+      </Row>
+    </>
   )
 }
