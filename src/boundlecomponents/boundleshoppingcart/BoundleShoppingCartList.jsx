75587edcb839fe440932b0d2f49aba4d51c2514a
unknown <cristian.ibanez@facturaenlinea.cl>
2023-12-02 22:22:39 -0300

    modifica boundleshoppingcartlist con get list desde storage

--- src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartList.jsx
+++ src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartList.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShoppingContext } from '../../context/ShoppingContext'
 
 export const BoundleShoppingCartList = () => {
+  const { shoppingCount, setShoppingCount } = useContext(ShoppingContext)
+  const [ arrayStorage, setArrayStorage ] = useState()
+
+  useEffect(() => {
+    ShoppingListStart()
+  }, [])
+
+
+  const ShoppingListStart = async () => {
+    let shoppingList = []
+    const ShoppingListGet = await localStorage.getItem('shoppingList')
+    console.log(ShoppingListGet)
+    if (ShoppingListGet === null) {
+      console.log('storage vacio')
+      setShoppingCount(null)
+    } else {
+      console.log('storage con registros')
+      shoppingList = JSON.parse(ShoppingListGet)
+      let shoppingListSize = shoppingList.length
+      setShoppingCount(shoppingListSize)
+      setArrayStorage(shoppingList)
+      console.log(typeof shoppingList)
+    }
+  }
   return (
-    <div>BoundleShoppingCartList</div>
+    <div>hola</div>
   )
 }
