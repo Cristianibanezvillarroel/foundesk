f59a1af89bd6bbb06edaedde46b69f2b16b5b246
unknown <cristian.ibanez@facturaenlinea.cl>
2023-12-04 13:01:47 -0300

    modifica etiqueta en summary cart y modifica estilos

--- src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartSummary.jsx
+++ src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartSummary.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Button, Container, Row } from 'react-bootstrap'
 
 export const BoundleShoppingCartSummary = () => {
   return (
     <>
-      <Container className='shopping-cart-summary-box'>
+      <div className='shopping-cart-summary-box'>
         <h3>Resumen</h3>
         <hr />
         <p>1 Producto en el carro</p>
@@ -14,7 +14,7 @@ export const BoundleShoppingCartSummary = () => {
           <div>CLP12000</div>
         </h4>
         <h5>incluye 1200 de iva</h5>
-      </Container>
+      </div>
       <div>he leido y aceptado los terminos y condiciones</div>
       <Button variant='primary'>PASAR POR LA CAJA</Button>
       <div>Volver a la tienda</div>
