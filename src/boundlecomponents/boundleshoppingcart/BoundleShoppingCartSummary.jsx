3452f6cb75a41c8f63906cfddda60ddeb8583fd5
unknown <cristian.ibanez@facturaenlinea.cl>
2023-12-04 12:14:31 -0300

    modifica tamaño trash carro de compra y agrega secciones a summary

--- src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartSummary.jsx
+++ src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartSummary.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export const BoundleShoppingCartSummary = () => {
   return (
-    <div>BoundleShoppingCartSummary</div>
+    <>
+    <div>Resumen</div>
+    <div>1 Producto en el carro</div>
+    <div>
+      <div>Total</div>
+      <div>CLP12000</div>
+    </div>
+    <div>incluye 1200 de iva</div>
+    <div>he leido y aceptado los terminos y condiciones</div>
+    <Button variant='primary'>PASAR POR LA CAJA</Button>
+    <div>Volver a la tienda</div>
+    </>
+    
   )
 }
