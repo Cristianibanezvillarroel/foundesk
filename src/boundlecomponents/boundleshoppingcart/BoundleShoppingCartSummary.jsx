6bbab67623d6799e40cee3300e2f92f7b7311dcf
unknown <cristian.ibanez@facturaenlinea.cl>
2023-12-04 16:48:30 -0300

    modifica formato a NumericFormat

--- src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartSummary.jsx
+++ src/boundlecomponents/boundleshoppingcart/BoundleShoppingCartSummary.jsx
@@ -6,6 +6,7 @@ import { NumericFormat } from 'react-number-format'
 
 export const BoundleShoppingCartSummary = () => {
   const { shoppingCount, setShoppingCount, shoppingAmount, setShoppingAmount } = useContext(ShoppingContext)
+  const shoppingIva = (shoppingAmount - shoppingAmount / 1.19)
   const navigate = useNavigate()
   const navigateCourses = () => {
     navigate('/boundlecourses')
@@ -16,16 +17,20 @@ export const BoundleShoppingCartSummary = () => {
       <div className='shopping-cart-summary-box'>
         <h3>Resumen</h3>
         <hr />
-        <div style={{textAlign: 'center'}}>
-          <div id='shopping-cart-summary-box-count'>{shoppingCount} Productos </div>
-          <div style={{textAlign: 'center'}}> &nbsp; en el carro</div>
+        <div style={{ textAlign: 'center' }}>
+          <div id='shopping-cart-summary-box-count'>{shoppingCount} Productos</div>
+          <div style={{ textAlign: 'center' }}>&nbsp; en el carro</div>
         </div>
         <hr />
         <h4>
-          <div>Total</div>
-          <NumericFormat value={shoppingAmount} thousandSeparator="." />
+          <div>Total </div>
+          <div>CLP$<NumericFormat value={shoppingAmount} thousandSeparator="." decimalSeparator="," /><NumericFormat value={shoppingAmount} thousandSeparator="." decimalSeparator="," /></div>
         </h4>
-        <h5>incluye 1200 de iva</h5>
+        <div id='shopping-cart-summary-iva'>
+          <h6>incluye&nbsp;</h6>
+          <NumericFormat value={shoppingIva} thousandSeparator="." decimalSeparator="," decimalScale={0} />
+          <h6>&nbsp;de iva</h6>
+        </div>
       </div>
       <div className='shopping-cart-summary-bottom'>
         <Button variant='success'>PAGAR</Button>
